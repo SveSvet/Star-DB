19f184cd2e1ffee26cb579a9630804d52b65ea00
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2020-06-02 12:38:36 +0300

    Разбираемся с Fetch и с Async/Await

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,1 +1,21 @@
-console.log('hello, world!');
+// обычный синтаксис fetch
+fetch('https://swapi.dev/api/people/1/')
+    .then((res) => {
+        return res.json();
+    })
+    .then((body) => {
+        console.log(body);
+    });
+
+// синтаксис async/await: создаем функцию для возвращения
+// тела любых запросов (запросы передаются по url)
+
+const getResourse = async(url) => {
+    const res = await fetch(url);
+    return await res.text();
+};
+
+getResourse('https://swapi.dev/api/people/1/')
+    .then((body) => {
+        console.log(body)
+    });
