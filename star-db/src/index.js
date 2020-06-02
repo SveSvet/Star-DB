551e95fb746039c26e568fe161a1e7ad15cdd327
Sveta Mituykhina <s.tu.tu.908@gmail.com>
2020-06-02 17:44:28 +0300

    Создание апи-клиента для запросов к серверу

--- star-db/src/index.js
+++ star-db/src/index.js
@@ -1,15 +1,50 @@
-const getResourse = async(url) => {
-    const res = await fetch(url);
-    if (!res.ok) {
-        throw new Error(`Could not fetch ${url} received ${res.status}`)
+class SwapiServise {
+    _apiBase = 'https://swapi.dev/api';
+
+    async getResourse(url) {
+        const res = await fetch(`${this._apiBase}${url}`);
+        if (!res.ok) {
+            throw new Error(`Could not fetch ${this._apiBase}${url} received ${res.status}`)
+        }
+        return await res.json();
     }
-    return await res.json();
+
+    async getAllPeople() {
+        const res = await this.getResourse(`/people/`);
+        return res.results;
+    };
+    getPerson(id) {
+        return this.getResourse(`/people/${id}`)
+    };
+
+    async getAllPlanets() {
+        const res = await this.getResourse(`/planets/`);
+        return res.results;
+    };
+    getPlanets(id) {
+        return this.getResourse(`/planets/${id}`)
+    };
+
+    async getAllPStarships() {
+        const res = await this.getResourse(`/starships/`);
+        return res.results;
+    };
+    getStarship(id) {
+        return this.getResourse(`/starships/${id}`)
+    };
 };
 
-getResourse('https://swapi.dev/api/people/156/')
-    .then((body) => {
-        console.log(body)
-    })
-    .catch((err) => {
-        console.error('Couls not fetch:', err)
-    });
+const swapi = new SwapiServise();
+swapi.getAllPeople().then((people) => {
+   people.forEach((p) => {
+       console.log(p.name)
+   })
+});
+
+swapi.getPerson(7).then((p) => {
+    console.log(p.name)
+});
+
+swapi.getAllPlanets().then((planets) => {
+    console.log(planets)
+})
